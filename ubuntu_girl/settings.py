5460cf99e941836f2450368e7d9d25da6243b985
Mesia Sipes <mesiasipes@gmail.com>
2017-06-12 18:03:47 -0600

    add blog app and Post class

--- ubuntu_girl/settings.py
+++ ubuntu_girl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^wzyam*5ai7u8aqvqz-xalot%k%mn4+wj6j%d33^u=a!f-d!^+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
